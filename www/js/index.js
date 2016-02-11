f6ebb22893a192cd780aa32d5e89606be289e00d
mediaria <mediaria@mediaria.com>
2016-02-11 03:08:08 +0100

    i

---
+++ www/js/index.js
@@ -0,0 +1,51 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+
+    },
+    onDeviceReady: function() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+        cordova.plugins.notification.badge.set(0);
+    }
+};
+
+window.onload=function(){
+    document.getElementById("foto").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("foto").style.height = (window.innerWidth-50)+"px";
+    document.getElementById("foto").style.backgroundImage="url('img/cordova.png')";
+    document.getElementById("foto").style.backgroundSize="50% 50%";
+};
+
+function capturePhoto() {
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 90,
+        destinationType: Camera.DestinationType.DATA_URL,
+        correctOrientation: true,
+        targetWidth: 1000,
+        targetHeight: 1000
+    });
+}
+
+function getPhoto(source) {
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 90,
+        destinationType: destinationType.FILE_URI,
+    sourceType: source });
+}
+
+function onSuccess(imageData) {
+    document.getElementById("foto").style.backgroundImage="url('data:image/jpeg;base64,"+imageData+"')";
+    document.getElementById("foto").style.backgroundSize="100% 100%";
+    cordova.plugins.notification.badge.increase();
+}
+
+function onPhotoURISuccess(imageURI) {
+    document.getElementById("foto").style.backgroundImage="url('"+imageURI+"')";
+    document.getElementById("foto").style.backgroundSize="100% 100%";
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
